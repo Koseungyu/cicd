924da07bad68177123f1d4eea9ec455d08534ae9
질문KoSeungyu < koeul123@gmail.com>
2022-07-06 11:38:43 +0900

    test

--- src/main/java/com/project/dogfaw/common/exception/CustomException.java
+++ src/main/java/com/project/dogfaw/common/exception/CustomException.java
@@ -10,6 +10,6 @@ public class CustomException extends RuntimeException {
     public CustomException(ErrorCode errorCode) {
         super(errorCode.getErrorMessage());
         this.errorCode = errorCode;
-        
+
     }
 }
